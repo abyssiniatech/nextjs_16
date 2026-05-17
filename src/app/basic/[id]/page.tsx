ca79ead68736dfe35d4cdad4d8a99ccaf2722e31
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-17 15:57:00 +0300

    add params in the basic components:

--- src/app/basic/[id]/page.tsx
+++ src/app/basic/[id]/page.tsx
@@ -1,10 +1,10 @@
 import style from "./basic.module.css"
-export default  function Basic(){
-   
+export default async function Basic({params}:{params:Promise<{id:string}>}){
+    const {id} =await params
     return(
         <section className="bg-indigo-900 text-white min-h-screen flex flex-col items-center justify-center">
         <div>
-            <h1 className={style.title}>Day1:</h1>
+            <h1 className={style.title}>Day1:{id}</h1>
         </div>
         </section>
     )
